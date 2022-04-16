f90c5e4bd24ce1d48f41e0a2b69c51edfd98e438
Adam Niziołek <niziolek.adam@gmail.com>
2022-04-16 13:57:35 +0200

    Deleted testing log

--- src/firebase.js
+++ src/firebase.js
@@ -4,7 +4,6 @@ import { getFirestore } from 'firebase/firestore'
 import { getAuth } from "firebase/auth"
 import { getStorage } from "firebase/storage"
 
-console.log("Zmienne środowiskowe: ",process.env);
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 export const auth = getAuth(app);
