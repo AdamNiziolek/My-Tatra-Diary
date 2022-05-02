4398fe93a739fe1871fca1df8c0c126f5eef70d2
Adam Niziołek <niziolek.adam@gmail.com>
2022-05-02 02:53:13 +0200

    Changed time calculation method

--- src/components/Statistics/StatisticsSection/StatisticsSection.js
+++ src/components/Statistics/StatisticsSection/StatisticsSection.js
@@ -1,42 +1,26 @@
 import React from 'react';
 
+const milisecondsInDay = 24 * 60 * 60 * 1000;
+const milisecondsInHour = 60 * 60 * 1000;
+
 export default function StatisticsSection({ entries }) {
   // Last time in Tatras
-  const lastEntryDate = entries[entries.length - 1].date;
-  const lastEntryYear = parseInt(lastEntryDate.substring(0, 4), 10);
-  const lastEntryMonth = parseInt(lastEntryDate.substring(5, 7), 10) - 1;
-  const lastEntryDay = parseInt(lastEntryDate.substring(8, 10), 10);
-  const lastDay = new Date(lastEntryYear, lastEntryMonth, lastEntryDay);
+  const lastEntryDate = new Date(entries[entries.length - 1].date);
   const todayDate = new Date();
-  const amountOfDays = Math.floor((todayDate - lastDay) / 86400000);
+  const amountOfDays = Math.floor(
+    (todayDate - lastEntryDate) / milisecondsInDay,
+  );
 
   // All time in Tatras
   let allTime = 0;
-  let timeStartHour;
-  let timeStartMinutes;
-  let timeEndHour;
-  let timeEndMinutes;
   entries.forEach(({ timeStart, timeEnd }) => {
-    if (timeStart.length === 4) {
-      timeStartHour = parseInt(timeStart.substring(0, 1), 10);
-      timeStartMinutes = parseInt(timeStart.substring(2, 4), 10);
-    } else {
-      timeStartHour = parseInt(timeStart.substring(0, 2), 10);
-      timeStartMinutes = parseInt(timeStart.substring(3, 5), 10);
-    }
-    if (timeEnd.length === 4) {
-      timeEndHour = parseInt(timeEnd.substring(0, 1), 10);
-      timeEndMinutes = parseInt(timeEnd.substring(2, 4), 10);
-    } else {
-      timeEndHour = parseInt(timeEnd.substring(0, 2), 10);
-      timeEndMinutes = parseInt(timeEnd.substring(3, 5), 10);
-    }
-
-    allTime =
-      allTime +
-      timeEndHour -
-      timeStartHour +
-      (timeEndMinutes - timeStartMinutes) / 60;
+    const timeStartDate = new Date();
+    timeStartDate.setTime(Date.parse(`Thu, 01 Jan 1970 ${timeStart}:00`));
+    const timeEndDate = new Date();
+    timeEndDate.setTime(Date.parse(`Thu, 01 Jan 1970 ${timeEnd}:00`));
+    const difference =
+      (timeEndDate.getTime() - timeStartDate.getTime()) / milisecondsInHour;
+    allTime += difference;
     return allTime;
   });
 
@@ -48,8 +32,8 @@ export default function StatisticsSection({ entries }) {
         <li>Times in the Tatra Mountains: {entries.length}</li>
         <li>Total time spent in the mountains: {Math.floor(allTime)} hours</li>
         <li>
-          Average duration of the trip: {Math.round(allTime / entries.length)}{' '}
-          hours
+          Average duration of the trip: {Math.round(allTime / entries.length)}
+          {' '} hours
         </li>
       </ul>
     </div>
